088e5fc409dd304dd5983986101688af5374c94d
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-26 19:51:49 +0800

    feat: create basic footer component

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,13 @@
+import BaseContainer from './BaseContainer'
+
+function Footer() {
+  return (
+    <footer className='border border-t'>
+      <BaseContainer>
+        <h1>MyAnime</h1>
+      </BaseContainer>
+    </footer>
+  )
+}
+
+export default Footer
