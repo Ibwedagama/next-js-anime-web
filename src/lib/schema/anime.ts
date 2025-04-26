3251f50b0362b64811089bf60ef222ee56650e25
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-26 20:39:03 +0800

    feat (schema): add anime list schema

---
+++ src/lib/schema/anime.ts
@@ -0,0 +1,51 @@
+import { z } from 'zod'
+
+export const animeSchema = z.object({
+  mal_id: z.number(),
+  url: z.string(),
+  images: z.object({
+    webp: z.object({
+      image_url: z.string(),
+      small_image_url: z.string(),
+      large_image_url: z.string(),
+    }),
+  }),
+  trailer: z.object({
+    youtube_id: z.string().nullable(),
+    url: z.string().nullable(),
+    embed_url: z.string().nullable(),
+  }),
+  title: z.string(),
+  status: z.string(),
+  airing: z.boolean(),
+  synopsis: z.string(),
+  season: z.string().nullable(),
+  year: z.number().nullable(),
+  episodes: z.number().nullable(),
+  rating: z.string(),
+  score: z.number(),
+  scored_by: z.number(),
+  rank: z.number(),
+  genres: z
+    .object({
+      mal_id: z.number(),
+      type: z.string(),
+      name: z.string(),
+      url: z.string(),
+    })
+    .array(),
+})
+
+export const animeListSchema = z.object({
+  data: z.array(animeSchema),
+  pagination: z.object({
+    last_visible_page: z.number(),
+    has_next_page: z.boolean(),
+    current_page: z.number(),
+    items: z.object({
+      count: z.number(),
+      total: z.number(),
+      per_page: z.number(),
+    }),
+  }),
+})
