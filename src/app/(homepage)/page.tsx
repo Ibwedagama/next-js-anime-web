516a3b02feeb1226fe0b1f4de9005feb5a24783b
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-26 20:38:30 +0800

    chore: move homepage to folder

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,10 +1,15 @@
 import BaseContainer from '@/components/common/BaseContainer'
+import { getAnimeList } from '@/lib/api/anime'
 
-export default function Home() {
+export default async function Home() {
+  const { data: animeList, error, status } = await getAnimeList()
+
   return (
     <main className='min-h-screen'>
       <BaseContainer className='py-8'>
         <h1>Homepage</h1>
+
+        <p>{JSON.stringify(animeList, null, 4)}</p>
       </BaseContainer>
     </main>
   )
