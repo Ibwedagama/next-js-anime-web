3509b726b70a7b92c1996c2ceae22575fe92aa7e
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-26 20:03:34 +0800

    feat: create fetch adapter function

---
+++ src/lib/api/fetcher.ts
@@ -0,0 +1,70 @@
+import { ZodSchema } from 'zod'
+import { getZodFlatErrorMessage } from '../utils'
+
+export type ApiResult<T> = {
+  data: T | null
+  error: string | null
+  message: string | null
+  status: number
+}
+
+type FetchJsonOptions = {
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
+  body?: unknown
+  headers?: HeadersInit
+}
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
+export async function fetchJson<T>(
+  url: RequestInfo,
+  schema: ZodSchema<T>,
+  options: FetchJsonOptions = {},
+): Promise<ApiResult<T>> {
+  try {
+    const { method = 'GET', body, headers } = options
+
+    const response = await fetch(`${BASE_URL}${url}`, {
+      method,
+      headers: { 'Content-Type': 'application/json', ...headers },
+      body: body ? JSON.stringify(body) : undefined,
+    })
+
+    if (!response.ok) {
+      const errorText = await response.text()
+      const { error } = JSON.parse(errorText)
+
+      return {
+        data: null,
+        error: `[FETCH ERROR] Failed to fetch ${url} with status ${response.status}: ${error}`,
+        message: null,
+        status: response.status,
+      }
+    }
+
+    const json = await response.json()
+    const parsed = schema.safeParse(json)
+
+    if (!parsed.success) {
+      throw new Error(
+        `[ZOD ERROR] Failed to parse response from ${url}: ${getZodFlatErrorMessage(
+          parsed.error,
+        )}`,
+      )
+    }
+
+    return {
+      data: parsed.data,
+      error: null,
+      message: json.message ?? null,
+      status: response.status,
+    }
+  } catch (error: any) {
+    return {
+      data: null,
+      error: `[FETCH ERROR] ${error.message}`,
+      message: null,
+      status: 500,
+    }
+  }
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { ZodError } from 'zod'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getZodFlatErrorMessage(error: ZodError) {
+  const { fieldErrors } = error.flatten()
+  return JSON.stringify(fieldErrors, null, 2)
 }
