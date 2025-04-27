b691d94bac59f8dda69ad987b30f39e4a223be03
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-27 16:17:20 +0800

    style (footer): adjust footer style

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,12 +1,18 @@
+import Link from 'next/link'
 import BaseContainer from './BaseContainer'
 
 function Footer() {
   return (
-    <footer className='border border-t'>
-      <BaseContainer>
-        <h1>MyAnime</h1>
+    <header className='sticky top-0 z-50 w-full bg-muted'>
+      <BaseContainer className='flex h-16 items-center justify-between'>
+        {/* Logo */}
+        <Link href='/' className='text-2xl font-bold'>
+          <div className='w-[75px] h-auto'>
+            <img src='/logo.png' alt='AniHub Logo' width={75} />
+          </div>
+        </Link>
       </BaseContainer>
-    </footer>
+    </header>
   )
 }
 
