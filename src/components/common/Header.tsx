ec44dac52a6889ca0b97ed3e22fc30bbfd4f97b5
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-27 15:48:24 +0800

    feat (navbar): remove links; add logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,62 +1,20 @@
-'use client'
-
 import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import { Menu } from 'lucide-react'
 import BaseContainer from './BaseContainer'
 import { Searchbar } from '../feature/search/Searchbar'
 
-const navLinks = [
-  { href: '/', label: 'Home' },
-  { href: '/about', label: 'About' },
-]
-
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 w-full border-b bg-background'>
+    <header className='sticky top-0 z-50 w-full bg-muted'>
       <BaseContainer className='flex h-16 items-center justify-between'>
         {/* Logo */}
         <Link href='/' className='text-2xl font-bold'>
-          MyAnime
+          <div className='w-[75px] h-auto'>
+            <img src='/logo.png' alt='AniHub Logo' width={75} />
+          </div>
         </Link>
 
-        {/* Desktop Menu */}
-        <nav className='hidden md:flex gap-6'>
-          {navLinks.map((link) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className='text-sm font-medium transition-colors hover:text-primary'
-            >
-              {link.label}
-            </Link>
-          ))}
-        </nav>
-
-        <Searchbar></Searchbar>
-
-        {/* Mobile Menu */}
-        <div className='md:hidden'>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant='outline' size='icon'>
-                <Menu className='h-6 w-6' />
-              </Button>
-            </SheetTrigger>
-            <SheetContent side='right' className='flex flex-col gap-6 pt-10'>
-              {navLinks.map((link) => (
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  className='text-lg font-medium'
-                >
-                  {link.label}
-                </Link>
-              ))}
-            </SheetContent>
-          </Sheet>
-        </div>
+        {/* Search */}
+        <Searchbar />
       </BaseContainer>
     </header>
   )
