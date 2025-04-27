6ff32c84ec1b56eed829014c32cbf8172eb8f4f8
Ibwedagama <wardana.wedagama@gmail.com>
2025-04-27 16:08:40 +0800

    style: adjust component style and color

--- src/components/feature/search/Searchbar.tsx
+++ src/components/feature/search/Searchbar.tsx
@@ -68,7 +68,7 @@ export function Searchbar() {
         <DialogTrigger asChild>
           <Button onClick={() => setOpen(true)}>
             Search
-            <kbd className='pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
+            <kbd className='pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
               <span className='text-xs'>⌘</span>K
             </kbd>
           </Button>
